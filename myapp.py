e772372a9be3af8188d181417c787ce58ec8a951
S <902418@cognizant.com>
2023-10-05 14:15:26 +0530

    initial commit

--- myapp.py
+++ myapp.py
@@ -11,11 +11,18 @@ def run_oc_command(command):
         return str(e)
 
 nodes_output = run_oc_command("oc get nodes")
-nodes_output = run_oc_command("oc get co")
+co_output = run_oc_command("oc get co")
 pods_output = run_oc_command("oc get pods -A | egrep -iv 'Running|Completed'")
+current_project_pods = run_oc_command(["oc get pods"])
 
 print("Nodes:")
 print(nodes_output)
 
 print("\nPods:")
 print(pods_output)
+
+print("\nCO:")
+print(pods_output)
+
+print("\npods in current project:")
+print(current_project_pods)
