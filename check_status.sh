9ab43df13c602e43725e537d2a6b559b8f621d93
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2023-10-06 13:15:40 +0530

    check_status edited

--- check_status.sh
+++ check_status.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-timestamp=$(date +'%Y%m%d_%H%M%S')
-
-output_log="status_check_${timestamp}_output.log"
-error_log="status_check_${timestamp}_error.log"
+output_log="status_check_output.log"
+#error_log="status_check_error.log"
 
 # Command 1
 get_nodes="oc get nodes"
-echo "Running Command 1: $get_nodes"
-$get_nodes >> "$output_log" 2>> "$error_log"
+echo "Running Command 1: $get_nodes" >>"$output_log"
+$get_nodes >> "$output_log"
 
 # Command 2
 get_co="oc get co'"
-echo "Running Command 2: $get_co"
-$get_co >> "$output_log" 2>> "$error_log"
+echo "Running Command 2: $get_co" >>"$output_log"
+$get_co >> "$output_log"
 
 # Command 3
-get_pods="oc get pods -A | egrep -v 'Running|Completed"
+get_pods="oc get pods -A | egrep -v 'Running|Completed" >>"$output_log"
 echo "Running Command 3: $get_pods"
-$get_pods >> "$output_log" 2>> "$error_log"
+$get_pods >> "$output_log" 
 
 echo "Commands executed. Output saved to '$output_log' and errors saved to '$error_log'"
