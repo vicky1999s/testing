1934bed864c7f91f947b4a539a76c7901ed60a0c
Student User <student@workstation.lab.example.com>
2023-10-09 03:24:53 -0400

    modified check_status.sh file

--- check_status.sh
+++ check_status.sh
@@ -4,18 +4,18 @@ output_log="status_check_output.log"
 #error_log="status_check_error.log"
 
 # Command 1
-get_nodes="oc get nodes"
-echo "Running Command 1: $get_nodes" >>"$output_log"
+get_nodes=$(oc get nodes)
+echo "Running Command 1: $get_nodes"
 $get_nodes >> "$output_log"
 
 # Command 2
-get_co="oc get co'"
-echo "Running Command 2: $get_co" >>"$output_log"
+get_co=$(oc get co)
+echo "Running Command 2: $get_co" 
 $get_co >> "$output_log"
 
 # Command 3
-get_pods="oc get pods -A | egrep -v 'Running|Completed" >>"$output_log"
+get_pods=$(oc get pods -A | egrep -v 'Running|Completed')
 echo "Running Command 3: $get_pods"
 $get_pods >> "$output_log" 
 
-echo "Commands executed. Output saved to '$output_log' and errors saved to '$error_log'"
+echo "Commands executed. Output saved to '$output_log'"
