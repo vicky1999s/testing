3353c822aeec59ec4f2331aa56a145ad273bcf47
S <902418@cognizant.com>
2023-10-11 12:39:14 +0530

    updated counter check in updated_script

--- updated_check_status.sh
+++ updated_check_status.sh
@@ -4,45 +4,65 @@ current_date=$(date +"%Y-%m-%d")
 filename_prefix="status_check_output"
 output_log="${filename_prefix}_${current_date}.log"
 
-#get nodes status
-get_nodes="oc get nodes"
-echo "checking node status" >> "$output_log"
-$get_nodes >> "$output_log" 2>&1
+(
+    #flock is a file locking command to ensure only one process is updating the file
+    flock -x 200
 
-#get clusteroperator status
-get_co="oc get co"
-echo "checking clusteroperator status" >> "$output_log"
-$get_co >> "$output_log" 2>&1
+    #get nodes status
+    get_nodes="oc get nodes"
+    echo "checking node status" >> "$output_log"
+    $get_nodes >> "$output_log" 2>&1
 
-#get pods status
-get_pods="oc get pods -A | egrep -v 'Running|Completed'"
-echo "checking pods status" >> "$output_log"
-eval "$get_pods" >> "$output_log" 2>&1
+    #get clusteroperator status
+    get_co="oc get co"
+    echo "checking clusteroperator status" >> "$output_log"
+    $get_co >> "$output_log" 2>&1
 
-echo "Commands executed. Output saved to '$output_log'"
+    #get pods status
+    get_pods="oc get pods -A | egrep -v 'Running|Completed'"
+    echo "checking pods status" >> "$output_log"
+    eval "$get_pods" >> "$output_log" 2>&1
+    
+) 200>"$output_log.lock"
 
+echo "Commands executed. Output and Errors saved to '$output_log'"
+
+#using shared counterfile to track the cronjob executions
 #increment the counterfile value, indicating that the cronjob is completed 
 increment_counter_file(){
-  counter_file="counterfile"
-  counter=$(cat "$counter_file")
-  max_count=8
+    counter_file="counterfile"
+    max_count=8
 
-  #check if the counter has reached the maximum limit
-  if ["$counter" -eq "$max_count"]; then
-    echo "All cronjobs have completed"
+    (
+        #flock is a file locking command to ensure only one process is updating the file
+        flock -x 201
+        
+        #To check if counterfile is present, else creates one and updated the value to 1
+        if [ -e "$counter_file" ]; then
+            counter=$(cat "$counter_file")
+        else
+            counter=1
+            echo "$counter" > "$counter_file"
+        fi
+        
 
-    #sending output to external system
-    ## WIP ##
+        #check if the counter has reached the maximum limit
+        if ["$counter" -eq "$max_count"]; then
+            echo "All cronjobs have completed"
 
-    #reset the counterfile to 0 to keep track of next cronjob execution
-    echo "0" > "$counter_file"
-  else
-    counter=$((counter+1))
-    #update the shared counter
-    echo "#counter" > "$counter_file"
+            #sending output to external system
+            ## WIP ##
 
-    echo "cronjob completed. counter value: $counter"
-  fi
+            #reset the counterfile to 0 to keep track of next cronjob execution
+            echo "0" > "$counter_file"
+        else
+            counter=$((counter+1))
+            #update the shared counter
+            echo "#counter" > "$counter_file"
+
+            echo "cronjob completed. counter value: $counter"
+        fi
+    ) 201>"$counter_file.lock" 
 }
 
 #move older files(2 days) from current directory to tmp directory
@@ -50,10 +70,10 @@ delete_old_files(){
   #local directory="/home/student/myapp/mygit/testing"
   local filename_prefix="$1"
   find . -type f -name "${filename_prefix}*" -mtime +2 -exec mv {} /tmp/ \;
-  echo "moved files older than 2 days with prefix '${filename_prefix}' in current directory"
+  echo "moved  the files that are older than 2 days with prefix '${filename_prefix}' in current directory"
 }
 
-###uncomment the below function calls to test
+###uncomment the below function calls for testing
 
 #increment_counter_file
 #delete_old_files "${filename_prefix}"
